4f6d0aa69460ea18c082220b0577ddbee47a621e
Laila Santos <lailaroberta100@gmail.com>
2022-06-27 20:05:36 -0300

    wip: somaTwo

--- src/main/kotlin/exercicios/soma.kt
+++ src/main/kotlin/exercicios/soma.kt
@@ -8,3 +8,14 @@ fun main(){
 
     println("O resultado da soma é: $soma")
 }
+
+//import java.util.* - soma com valores pedidos no input
+//fun main(args: Array<String>) {
+//    val sc = Scanner(System.`in`)
+//
+//    val A: String = sc.next()
+//    val B: String = sc.next()
+//    val soma = A.toInt() + B.toInt()
+//
+//    println("SOMA = $soma")
+//}
